87e78258b5436d26d3d77f6a367f4c751af50b83
dbaigara <dbaigara@cern.ch>
2023-04-03 12:59:53 +0300

    finished homework 03

---
+++ index.js
@@ -0,0 +1,60 @@
+// alert("Hi");
+
+// const user = {
+// };
+// let userName = prompt("What is your name?", "Dosbol");
+// user["userName"] = userName;
+// let isAdult = confirm("Are you 18 years old?");
+// user["isAdult"] = isAdult;
+// let userGender = prompt("Male/Female", "None");
+// user["userGender"] = userGender;
+// console.log(user);
+
+
+// let randNumber = prompt("Guess the number between 0 and 10?", "9");
+// if (randNumber >= 10) {
+//     alert("Your number more than 10");
+// } else if (randNumber < 0) {
+//     alert("Your number less than 0");
+// } else {
+//     alert(`You guessed ${randNumber}`);
+// }
+
+
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// switch (a) {
+//     case 0:
+//         alert(0);
+//         break;
+//     case 1:
+//         alert(1);
+//         break;
+//     case 2:
+//     case 3:
+//         alert('2, 3');
+//         break; 
+// }
+
+// let num = 0;
+// console.log(num)
+// // for (let i = 0; i <= 100; i += 2) {
+//     // console.log(i);
+//     // i = i + 1;
+//     num += i;
+// }
+// console.log(`Сумма всех четных чисел от 1-100 = ${num}`);
+
+
+// let i = 0;
+// while (i < 3) {
+//     alert(`number ${i}!`);
+//     i++;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     // alert( `number ${i}!` );
+//   }
